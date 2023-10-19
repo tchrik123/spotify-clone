21440402d881c3bc4076c55228f81e47d3f4cec4
nikoloz tchrikishvili <nikachrikishvili@gmail.com>
2023-10-19 18:15:20 +0400

    create list of songs and deshboard

---
+++ hooks/useUser.tsx
@@ -0,0 +1,19 @@
+import { Subscription, UserDetails } from "../types";
+import { createContext } from "react";
+import { User } from "@supabase/auth-helpers-nextjs";
+
+type UserContextType = {
+  accessToken: string | null;
+  user: User | null;
+  userDetails: UserDetails | null;
+  isLoading: boolean;
+  subscription: Subscription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export interface Props {
+  [propName: string]: any;
+}
